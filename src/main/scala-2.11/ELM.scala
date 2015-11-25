96d9279ffc1f389b870b99394d53dc5e5d608f87
felipe <fzschornack@gmail.com>
2015-11-25 11:40:57 -0300

    ADD: ELM algorithm

---
+++ src/main/scala-2.11/ELM.scala
@@ -0,0 +1,61 @@
+import org.apache.spark.mllib.linalg.{Vectors, Matrices}
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.rdd.RDD
+
+import java.util.Random
+
+import org.apache.spark.storage.StorageLevel
+
+/**
+ * Object to implement the "most creative problem solving" machine learning algorithm: Extreme Learning Machine
+ *
+ * Usage:
+ *
+ *  Train
+ *  1) originalLabeledPoints
+ *  2) newLabeledPoints = ELM.transform(originalLabeledPoints, 200, 1)
+ *  3) use newLabeledPoints to train a model
+ *
+ *  Prediction
+ *  4) originalPredictionLabeledPoints
+ *  5) newPredictionLabeledPoints = ELM.transform(originalPredictionLabeledPoints, 200, 1) // MUST BE THE SAME
+ *                                                                                         // SEED USED FOR TRAINING!!!
+ *  6) model.predict(newPredictionLabeledPoints)
+ *  7) open a champagne bottle, sit and relax.
+ *
+ * Created by fzschornack on 24/11/15.
+ */
+object ELM {
+
+  /**
+   * Transform input features in new hiddenLayerSize features using a non linear function.
+   *
+   * @param input
+   * @param hiddenLayerSize
+   * @return
+   */
+  def transformation(input: RDD[LabeledPoint], hiddenLayerSize: Int, seed: Int): RDD[LabeledPoint] = {
+    val numberOfFeatures = input.take(1).apply(0).features.size
+
+    val inputToHiddenLayerWeights = Matrices.randn(hiddenLayerSize, numberOfFeatures , new Random(seed))
+
+    input.persist(StorageLevel.MEMORY_AND_DISK)
+
+    val elmLabeledPoints = input.map { lp =>
+
+      val label = lp.label
+      val elmFeatures = inputToHiddenLayerWeights.multiply(lp.features) // wx
+        .values.map(logisticFunction) // g(wx)
+
+      LabeledPoint(label, Vectors.dense(elmFeatures))
+
+    }
+
+    elmLabeledPoints
+  }
+
+  private def logisticFunction(wx: Double): Double = {
+    1 / (1 + math.exp(-wx))
+  }
+
+}
